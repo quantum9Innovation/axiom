85dca3f8271c94919b459e2f3f1ef4715b6a6073
quantum9Innovation <quantum9Innovation@users.noreply.github.com>
2023-06-09 18:33:06 +0000

    deploy: 37e1e17ac2e0c9a8494ee4e8a4a57f47cc5cd62c

--- en/assets/js/mantequilla.js
+++ en/assets/js/mantequilla.js
@@ -19,47 +19,6 @@ const chromaInt = () => {
   document.head.appendChild(stylesheet)
 }
 
-const katex = () => {
-  const stylesheet = document.createElement('link')
-  stylesheet.rel = 'stylesheet'
-  stylesheet.type = 'text/css'
-  stylesheet.href = 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css'
-  stylesheet.integrity = 'sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC'
-  stylesheet.crossOrigin = 'anonymous'
-
-  const script = document.createElement('script')
-  script.src = 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.js'
-  script.integrity = 'sha384-X/XCfMm41VSsqRNQgDerQczD69XqmjOOOwYQvr/uuC+j4OPoNhVgjdGFwhvN02Ja'
-  script.crossOrigin = 'anonymous'
-
-  const render = document.createElement('script')
-  render.src = 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/contrib/auto-render.min.js'
-  render.integrity = 'sha384-+XBljXPPiv+OzfbB3cVmLHf4hdUFHlWNZN5spNQ7rmHTXpd7WvJum6fIACpNNfIR'
-  render.crossOrigin = 'anonymous'
-  render.defer = true
-
-  document.head.appendChild(stylesheet)
-  document.head.appendChild(script)
-  document.head.appendChild(render)
-
-  // KaTeX
-  // run after render loads and document body loads
-  render.onload = _e => {
-    renderMathInElement(document.body, {
-      // customized options
-      // • auto-render specific keys, e.g.:
-      delimiters: [
-        { left: '$$', right: '$$', display: true },
-        { left: '$', right: '$', display: false },
-        { left: '\\(', right: '\\)', display: false },
-        { left: '\\[', right: '\\]', display: true }
-      ],
-      // • rendering keys, e.g.:
-      throwOnError: false
-    })
-  }
-}
-
 const themes = (interactivesIDs, imgs) => {
   // Dark embed toggle
   let dark = false
